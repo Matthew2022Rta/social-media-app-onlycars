6c6e1f9455cb7dfece8b9da86b54ce2cd5d1fbd0
Matthew2022Rta <matthewramos2777@gmail.com>
2025-04-16 16:49:23 -0400

    Added unit tests for user

--- test/user.test.js
+++ test/user.test.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 import User from '../models/User.mjs';
 
-describe('User Model', function () {
-  before(async function () {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+describe('User Model', function () { 
+   before(async function () {
+     await mongoose.connect('mongodb://127.0.0.1:27017/test');
   });
 
   after(async function () {
@@ -11,12 +11,13 @@ describe('User Model', function () {
     await mongoose.disconnect();
   });
 
-  it('should be invalid with no username', function (done) {
-    const u = new User();
+   it('should be invalid with no username', function (done) {
+     const u = new User();
 
-    u.validate((err) => {
-      if (err && err.errors.username) done(); // ✅ test passes
-      else done(new Error('Expected username error'));
-    });
-  });
+     u.validate((err) => {
+       if (err && err.errors.username) done(); // ✅ test passes
+       else done(new Error('Expected username error'));
+     });
+   });
 });
+//added the user tests
